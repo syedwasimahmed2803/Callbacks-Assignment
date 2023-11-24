7f2124d088dfbcef959b0f25d257ec8e1056b838
Syed Wasim Ahmed <syedwasimahmed2803@gmail.com>
2023-11-24 16:14:06 +0530

    created Test file, more changes need to be made

--- test/testFsProblem1.cjs
+++ test/testFsProblem1.cjs
@@ -1,5 +1,5 @@
 const fsProblem1 = require("../fs-problem1.cjs");
 const path = require("path");
 
-fsProblem1(path.join(__dirname), Math.floor((Math.random() + 1) * 5));
-// fsProblem1(path.join(__dirname), 3);
+// fsProblem1(path.join(__dirname), Math.floor((Math.random() + 1) * 5));
+fsProblem1(path.join(__dirname), 3);
